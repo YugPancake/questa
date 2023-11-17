35ad134147dfe78c607864a632f140416dfce225
Teatov <hlebpoimenigleb@gmail.com>
2023-11-17 22:43:09 +1000

    feat(tailwind): добавлен цвет fantasy
    в tailwind.config.ts в тему был добавлен новый цвет fantasy: '#faf4ef'

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,20 +2,21 @@ import { fontFamily } from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {
-			colors: {
-				burgundy: '#780115',
-				flame: '#d7572a',
-				'sand-brown': '#ffb564',
-				sunset: '#ffd19d',
-				olive: '#918450'
-			}
-		},
-		fontFamily: {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        burgundy: '#780115',
+        flame: '#d7572a',
+        sand: '#ffb564',
+        sunset: '#ffd19d',
+        olive: '#918450',
+        fantasy: '#faf4ef',
+      },
+    },
+    fontFamily: {
       mono: ['Press Start', ...fontFamily.mono],
     },
-	},
-	plugins: []
+  },
+  plugins: [],
 };
