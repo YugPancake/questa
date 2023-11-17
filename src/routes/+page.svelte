956731ee00183d689ce10322948394da3ccf3b3c
Teatov <hlebpoimenigleb@gmail.com>
2023-11-17 23:00:34 +1000

    feat(главная): создана базовая разметка для главной страницы
    убраны закомментированные стили. создана панель для информации (статы персонажа, главные задачи,
    таймер). создана панель для досок задач

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,109 +1,38 @@
 <script lang="ts">
-	import line from '$lib/assets/img/line_1.svg';
+  import Container from '$lib/components/Container.svelte';
+
+  interface TaskBoard {
+    name: string;
+  }
+
+  const taskBoards: TaskBoard[] = [{ name: '1' }, { name: '2' }, { name: '3' }, { name: '4' }];
 </script>
 
 <svelte:head>
-	<title>Questa</title>
+  <title>Questa</title>
 </svelte:head>
 
-<body>
-	<div class="main">
-		<div class="info">
-			<div class="character" />
-			<div class="statbox">
-				<div class="name">Имя</div>
-				<img alt="line" src={line} class="line" />
-				<div class="class">Класс</div>
-			</div>
-		</div>
-		<div class="maintask" />
-		<div class="timer" />
-	</div>
+<div class="color-sunset">
+  <Container class="grid grid-cols-1 gap-6 py-6 lg:grid-cols-3">
+    <div class="color-fantasy rounded-xl p-6">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti blanditiis optio odit
+      doloremque beatae quas consequuntur perferendis enim pariatur tenetur.
+    </div>
+    <div class="color-fantasy rounded-xl p-6">m2</div>
+    <div class="color-fantasy rounded-xl p-6">m3</div>
+  </Container>
+</div>
 
-	<div class="tasks" />
-</body>
+<Container class="grow">
+  <div class="grid grid-cols-1 gap-6 py-6 sm:grid-cols-2 lg:grid-cols-3">
+    {#each taskBoards as taskBoard}
+      <div class="color-sunset rounded-xl p-6">
+        {taskBoard.name}
+      </div>
+    {/each}
 
-<style>
-	/* @font-face {
-		font-display: swap;
-		font-family: 'Press Start';
-		font-style: normal;
-		font-weight: 400;
-		src: url('$lib/assets/fonts/press-start-2p-v15-cyrillic_latin-regular.woff2') format('woff2'),
-			url('$lib/assets/fonts/press-start-2p-v15-cyrillic_latin-regular.ttf') format('truetype');
-	}
-
-	body {
-		font-family: Press Start;
-		background-color: #faf4ef;
-		margin: 0%;
-	}
-	.main {
-		display: flex;
-		padding-top: 90px;
-		height: 335px;
-		background-color: #ffd29d;
-		margin: 0%;
-	}
-	.info {
-		display: flex;
-		width: 560px;
-		height: 270px;
-		flex-shrink: 0;
-		border-radius: 15px;
-		background: #faf4ef;
-		margin-top: 32px;
-		margin-left: 155px;
-	}
-
-	.maintask {
-		width: 490px;
-		height: 270px;
-		flex-shrink: 0;
-		border-radius: 15px;
-		background: #faf4ef;
-		margin-top: 32px;
-		margin-left: 70px;
-	}
-	.timer {
-		width: 420px;
-		height: 270px;
-		flex-shrink: 0;
-		border-radius: 15px;
-		background: #faf4ef;
-		margin-top: 32px;
-		margin-left: 70px;
-	}
-
-	.character {
-		width: 200px;
-		height: 227px;
-		flex-shrink: 0;
-		border-radius: 15px;
-		border: 3px solid #918450;
-		margin-top: 23px;
-		margin-left: 23px;
-	}
-	.statbox {
-		margin-top: 28px;
-		margin-left: 25px;
-	}
-	.name {
-		color: #780116;
-
-		font-size: 32px;
-		font-style: normal;
-		font-weight: 400;
-		line-height: normal;
-	}
-
-	.line {
-		padding: 0;
-	}
-	.class {
-		color: #d8572a;
-		font-family: Press Start 2P;
-		font-size: 16px;
-		padding-top: 18px;
-	} */
-</style>
+    <div>
+      <button class="btn color-sunset text-flame">Добавить доску</button>
+    </div>
+  </div>
+</Container>
