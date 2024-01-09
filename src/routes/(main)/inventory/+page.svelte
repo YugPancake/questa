30eadb4ab5f7957a3748c863bc1ef36a52f05a23
YugPancake <yug.blet@gmail.com>
2024-01-09 11:45:43 +1000

    feat(фронтенд): оформлена страница инвентаря

--- src/routes/(main)/inventory/+page.svelte
+++ src/routes/(main)/inventory/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-  import CharacterSheet from '$lib/components/CharacterSheet.svelte';
   import Container from '$lib/components/Container.svelte';
+  import search from '$lib/assets/icons/search.svg?raw';
+  import CharacterSheet from '$lib/components/CharacterSheet.svelte';
   import type { PageData } from './$types';
 
   export let data: PageData;
 
-  const { user } = data;
+  let { user } = data;
 </script>
 
 <svelte:head>
@@ -17,10 +18,36 @@
     <div class="color-fantasy flex gap-6 rounded-xl p-6 lg:h-64">
       <CharacterSheet {user} />
     </div>
-    <div class="color-fantasy rounded-xl p-6 lg:h-64">
-      <p class="text-2xl">Инвентарь</p>
-    </div>
   </Container>
 </div>
 
-<Container class="grow"></Container>
+<Container class="flex grow flex-col gap-6 py-6">
+  <form class="flex justify-end">
+    <div class="flex w-full max-w-sm">
+      <input
+        type="search"
+        id="search"
+        class="input rounded-r-none border-r-0"
+        placeholder="Поиск..."
+      />
+      <dir
+        class="input my-0 flex w-fit items-center justify-center rounded-l-none border-l-0 p-0 text-flame"
+      >
+        <button type="submit" class="clickable p-3">
+          {@html search}
+        </button>
+      </dir>
+    </div>
+  </form>
+  <div class="flex grow flex-col items-stretch gap-6 md:flex-row">
+    <div class="rounded-xl border-2 border-burgundy p-6">
+      <p>Фильтрация</p>
+    </div>
+    <div class="grow rounded-xl border-2 border-burgundy p-6">
+      <div class="px-8 grid grid-cols-5 grid-rows-3">
+        <div class="w-36 h-36 rounded-xl border-4 border-olive object-cover mx-8 my-12" ></div>
+      </div>
+    </div>
+  </div>
+</Container>
+
